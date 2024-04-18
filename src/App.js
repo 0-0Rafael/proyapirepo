715cba0c1f558f1c61dfcb7f2433493fcf9fb24c
Baljeet <rafaelarielcg@gmail.com>
2024-04-18 13:02:49 -0400

    First commit, With First Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function realizarOperacion(e){
+  e.preventDefault();
+  const v1 = parseInt(e.target.valor1.value);
+  const v2 = parseInt(e.target.valor2.value);
+  const sumar = v1 + v2;
+  alert("la suma es: " + sumar );
+  
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <form onSubmit={realizarOperacion}>
+        <p>Ingresa el primer valor: 
+          <input type='number' name='valor1'/>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <p>Ingresa el segundo valor: 
+          <input type='number' name='valor2'/>
+        </p>
+
+        <p>
+          <input type='submit' name='sumar'/>
+        </p>
+      </form>
     </div>
   );
+    
 }
 
 export default App;
---
+++ src/components/LoginPComponent.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LoginPComponent = () => {
+  return (
+    <div>LoginPComponent</div>
+  )
+}
